9a4bc2f80358741040437bc9a53618c2bc38313e
Ikey Doherty <ikey@solus-project.com>
2018-01-31 12:38:20 +0000

    Begin skeleton for new C-based cpu service
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ cpu-on-demand/main.c
@@ -0,0 +1,22 @@
+/*
+ * This file is part of solus-hardware-config.
+ *
+ * Copyright © 2018 Solus Project
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Trivial helper to silence warnings */
+#define __cod_unused__ __attribute__((unused))
+
+int main(__cod_unused__ int argc, __cod_unused__ char **argv)
+{
+        fputs("Not yet implemented\n", stderr);
+        return EXIT_SUCCESS;
+}
