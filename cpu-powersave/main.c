ba762a110eab5d20dccedc85e524b07dbb3235f4
Ikey Doherty <ikey@solus-project.com>
2018-02-01 11:41:46 +0000

    cpu-powersave: Add missing modelines
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- cpu-powersave/main.c
+++ cpu-powersave/main.c
@@ -271,3 +271,16 @@ int main(int argc, char **argv)
         fprintf(stderr, "Unknown argument '%s'\n", subarg);
         return EXIT_FAILURE;
 }
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
